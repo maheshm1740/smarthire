5a84100c36c13b873f03bd3d1e213f2ff09163e5
Mahesh M <maheshdm1740@gmail.com>
2026-03-30 12:54:36 +0530

    main

---
+++ infra/postgres/init-multiple-dbs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Creates multiple PostgreSQL databases from the POSTGRES_MULTIPLE_DATABASES env var
+# Usage: POSTGRES_MULTIPLE_DATABASES=auth_db,job_db
+
+set -e
+set -u
+
+function create_user_and_database() {
+	local database=$1
+	echo "Creating database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE DATABASE $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	echo "Multiple databases requested: $POSTGRES_MULTIPLE_DATABASES"
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+		create_user_and_database $db
+	done
+	echo "All databases created."
+fi
